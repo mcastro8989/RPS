31a4313e874f42e65dff11eef18c80df5ded0592
mcastro8989 <manuel_castro45@outlook.com>
2024-08-16 06:15:49 -0700

    wrote code for human input. Code still not returning correct value

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 
-function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+// function getComputerChoice() {
+//     return Math.floor(Math.random() * 3);
     
-}
+// }
 
-let computerChoice = getComputerChoice()
+// let computerChoice = getComputerChoice()
 
 
-if (computerChoice == 0) {
-     console.log("Rock"); 
- } else if (computerChoice == 1) {
-     console.log("Paper");
- } else {
-     console.log("Scissors");
- }
+// if (computerChoice == 0) {
+//      console.log("Rock"); 
+//  } else if (computerChoice == 1) {
+//      console.log("Paper");
+//  } else {
+//      console.log("Scissors");
+//  }
+
+let answer = prompt("Please enter either 'Rock', 'Paper', or 'Scissors': ").toLowerCase
+
+let humanChoice = getHumanChoice(answer)
+
+  function getHumanChoice() {
+       if (answer == "rock") {
+        console.log("You Picked Rock!");
+       } else if (answer == "paper") {
+        console.log("You Picked Paper");
+       } else {
+        console.log("You Picked Scissors!");
+       }
+  }
+
+
+
+
