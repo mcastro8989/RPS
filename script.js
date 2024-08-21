bf8988bc972797c59a200f2a9bcdcb64357911b5
mcastro8989 <manuel_castro45@outlook.com>
2024-08-20 20:11:34 -0700

    creat function for 'play round'. Code still needs few adjustments to work prooperly

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
-// function getComputerChoice() {
-//     return Math.floor(Math.random() * 3);
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
     
-// }
+}
 
-// let computerChoice = getComputerChoice()
+let computerChoice = getComputerChoice();
 
 
-// if (computerChoice == 0) {
-//      console.log("Rock"); 
-//  } else if (computerChoice == 1) {
-//      console.log("Paper");
-//  } else {
-//      console.log("Scissors");
-//  }
+if (computerChoice == 0) {
+     console.log("Rock"); 
+ } else if (computerChoice == 1) {
+     console.log("Paper");
+ } else {
+     console.log("Scissors");
+ }
 
 let answer = prompt("Please enter either 'Rock', 'Paper', or 'Scissors': ").toLowerCase();
 
@@ -36,4 +36,28 @@ let humanScore = 0
 let computerScore = 0
 
 
+function playRound(humanChoice, computerChoice) {
+     if (humanChoice == "rock" && computerChoice =="scissors") {
+          console.log("You Win! Rock Beats Scissors!");
+     } else if (humanChoice == "rock" && computerChoice == "paper") {
+          console.log("You Lose! Paper Beats Rock!");
+     } else if (humanChoice == "rock" && computerChoice == "rock") {
+          console.log("Its a Tie!");
+     } else if (humanChoice == "paper" && computerChoice == "scissors") {
+          console.log("You Lose! Scissors Beats Paper!");
+     } else if (humanChoice == "paper" && computerChoice == "rock") {
+          console.log("You Win! Paper Beats Rock!"); 
+     } else if (humanChoice == "paper" && computerChoice == "paper") {
+          console.log("Its a Tie!");
+     } else if (humanChoice == "scissors" && computerChoice == "paper") {
+          console.log("You Win! Scissors Beats Paper");
+     } else if (humanChoice == "scissors" && computerChoice == "rock") {
+          console.log("You Lose! Rock Beats Scissors");
+     } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+          console.log("Its a Tie!");
+     } else {
+          console.log("Thats Not How You Play!");
+     }
+}
 
+playRound(humanChoice, computerChoice);
